90349c54d6a7514860f9ecb400591738b5fe1821
Andy Ledvina <andy@flexport.com>
2014-10-15 00:33:05 -0700

    generate some handlers using my templates

---
+++ apps/bam_api/src/index_handler.erl
@@ -0,0 +1,23 @@
+-module(index_handler).
+
+-export([init/3]).
+-export([allowed_methods/2, content_types_provided/2]).
+-export([get_json/2]).
+
+init(_, _Req, _Opts) ->
+  {upgrade, protocol, cowboy_rest}.
+
+allowed_methods(Req, State) ->
+  Methods = [
+    <<"GET">>
+  ],
+  {Methods, Req, State}.
+
+content_types_provided(Req, State) ->
+  JSON = {{<<"application">>, <<"json">>, '*'}, get_json},
+  ContentTypes = [JSON],
+  {ContentTypes, Req, State}.
+
+get_json(Req, State) ->
+  Result = <<"{\"hello\": \"world\"}">>,
+  {Result, Req, State}.
---
+++ apps/bam_api/src/service_handler.erl
@@ -0,0 +1,37 @@
+-module(service_handler).
+
+-export([init/3, rest_init/2]).
+-export([allowed_methods/2, content_types_provided/2, content_types_accepted/2]).
+-export([get_json/2, from_json/2]).
+
+init(_, _Req, _Opts) ->
+  {upgrade, protocol, cowboy_rest}.
+
+rest_init(Req, Opts) ->
+  State = [],
+  {ok, Req, State}.
+
+allowed_methods(Req, State) ->
+  Methods = [
+    <<"GET">>,
+    <<"PUT">>,
+    <<"POST">>
+  ],
+  {Methods, Req, State}.
+
+content_types_provided(Req, State) ->
+  JSON = {{<<"application">>, <<"json">>, '*'}, get_json},
+  ContentTypes = [JSON],
+  {ContentTypes, Req, State}.
+
+content_types_accepted(Req, State) ->
+  JSON = {{<<"application">>, <<"json">>, []}, from_json},
+  ContentTypes = [JSON],
+  {ContentTypes, Req, State}.
+
+get_json(Req, State) ->
+  Result = <<"{\"hello\": \"world\"}">>,
+  {Result, Req, State}.
+
+from_json(Req, State) ->
+  {true, Req, State}.
