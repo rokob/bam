1e7986e2e8ce3b74981b71db9f85e5f4a6634339
Andy Ledvina <andy@flexport.com>
2014-10-16 23:44:00 -0700

    get the auth middleware to the point where it does something, proof of concept

--- apps/bam_api/src/auth_middleware.erl
+++ apps/bam_api/src/auth_middleware.erl
@@ -3,21 +3,29 @@
 
 -export([execute/2]).
 
+-define(AUTH_HEADER, <<"x-bam-auth-token">>).
+
 execute(Req, Env) ->
   case is_authorized(Req) of
     {authorized, Value} ->
-      {ok, Req, [{authorized, Value} | Env]};
+      Req2 = cowboy_req:set_meta(authorized, Value, Req),
+      {ok, Req2, Env};
     {error, StatusCode} ->
       {error, StatusCode, Req}
   end.
 
 is_authorized(Req) ->
   case check_header(Req) of
-    ok ->
-      {authorized, true};
-    _  ->
-      {error, 401}
+    false  ->
+      {error, 401};
+    Value ->
+      {authorized, Value}
   end.
 
-check_header(_Req) ->
-  ok.
+check_header(Req) ->
+  case cowboy_req:header(?AUTH_HEADER, Req, undefined) of
+    {undefined, _Req2} ->
+      false;
+    {Value, _Req2} ->
+      Value
+  end.
