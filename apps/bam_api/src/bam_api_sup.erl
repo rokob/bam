9c48545e35894f0b7aafd9f9882d74ae213e0022
Andy Ledvina <andy@flexport.com>
2014-10-21 23:21:48 -0700

    get rid of stupid comments

--- apps/bam_api/src/bam_api_sup.erl
+++ apps/bam_api/src/bam_api_sup.erl
@@ -2,25 +2,18 @@
 
 -behaviour(supervisor).
 
-%% API
 -export([start_link/0]).
 
-%% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
-%% ===================================================================
-%% API functions
-%% ===================================================================
+% API
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
+% Callbacks
 
 init([]) ->
     {ok, { {one_for_one, 5, 10}, []} }.
