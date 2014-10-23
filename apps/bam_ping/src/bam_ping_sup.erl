c0a7100424b155355938e07c3a6ff668cb645747
Andy Ledvina <andy@flexport.com>
2014-10-23 08:35:49 -0700

    add the event system to the root supervisor's tree

--- apps/bam_ping/src/bam_ping_sup.erl
+++ apps/bam_ping/src/bam_ping_sup.erl
@@ -4,18 +4,17 @@
 
 -export([start_link/0]).
 
-%% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type, Depends), {I, {I, start_link, []}, permanent, 2000, Type, Depends}).
 
 start_link() ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
   Children = [
-    ?CHILD(bam_ping_worker_sup, supervisor)
+    ?CHILD(bam_ping_worker_sup, supervisor, [bam_ping_worker_sup, bam_ping_worker]),
+    ?CHILD(bam_ping_event, worker, [bam_ping_event])
   ],
-  {ok, { {one_for_one, 5, 10}, Children} }.
-
+  RestartStrategy = {one_for_one, 4, 3600},
+  {ok, {RestartStrategy, Children}}.
