877cb8b0a5b61be8313d5360566a78479ff78c72
Andy Ledvina <andy@flexport.com>
2014-10-22 19:57:35 -0700

    move the milliseconds function to lib for now, will actually do something over there at some point

--- apps/bam_lib/src/bam_lib.erl
+++ apps/bam_lib/src/bam_lib.erl
@@ -1,12 +1,16 @@
 -module(bam_lib).
 
--export([my_func/0]).
+-export([my_func/0, now_milliseconds/0]).
 
 %% API
 
 my_func() ->
   io:format("~p~n", [bam_conf:get_val(bam_lib, whoa, answer, 69)]),
   ok().
+
+now_milliseconds() ->
+  {Mega, Sec, Micro} = os:timestamp(),
+  (Mega*1000000 + Sec)*1000 + round(Micro/1000).
 
 %% Internals
 
