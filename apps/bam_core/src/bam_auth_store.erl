f0e50c8724e342566d0585db9862e67251b4b35c
Andy Ledvina <andy@flexport.com>
2014-11-06 09:21:05 -0800

    use jwt for the auth stuff, still just playing around

--- apps/bam_core/src/bam_auth_store.erl
+++ apps/bam_core/src/bam_auth_store.erl
@@ -2,43 +2,48 @@
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
 
--export([start_link/0, new_token/2, verify_token/3]).
+-export([start_link/0, new_token/3, verify_token/2]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {tokens=[]}).
+-define(SALT, <<"ALLYOURBaSeAREBeLONGTOus">>).
+-record(state, {users}).
 
 % API
 
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-new_token(Username, Password) ->
-  gen_server:call(?SERVER, {token_request, Username, Password}).
+new_token(Username, Password, Key) ->
+  gen_server:call(?SERVER, {token_request, Username, Password, Key}).
 
-verify_token(Username, Token, Signature) ->
-  gen_server:call(?SERVER, {verify_token, Username, Token, Signature}).
+verify_token(Token, Key) ->
+  gen_server:call(?SERVER, {verify_token, Token, Key}).
 
 % Callbacks
 
 init([]) ->
-  State = #state{},
+  State = #state{users=orddict:new()},
   {ok, State}.
 
-handle_call({token_request, Username, Password}, _From, State=#state{tokens=Tokens}) ->
-  Token = <<"123">>,
-  Hash = Password,
-  Signature = [Username, Token],
-  NewState = State#state{tokens=[{Username, {Hash, Token, Signature}} | Tokens]},
-  Reply = [{token, Token}, {signature, Signature}],
-  {reply, ok, NewState};
-handle_call({verify_token, Username, Token, Signature}, _From, State=#state{tokens=Tokens}) ->
-  case proplists:get_value(Username, Tokens) of
-    {_, Token, Signature} ->
-      {reply, ok, State};
+handle_call({token_request, Username, Password, Key}, _From, State) ->
+  case check_username_password(Username, Password, State) of
+    ok ->
+      Payload = [{username, Username}, {expiration, get_expiration()}],
+      JWT = bam_jwt:encode(Payload, Key),
+      Reply = {ok, JWT},
+      {reply, Reply, State};
     _ ->
-      {reply, false, State}
+      {reply, error, State}
+  end;
+handle_call({verify_token, Token, Key}, _From, State) ->
+  case bam_jwt:decode(Token, Key) of
+    error ->
+      {reply, error, State};
+    Payload ->
+      Reply = {ok, Payload},
+      {reply, Reply, State}
   end;
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
@@ -57,3 +62,25 @@ code_change(_OldVsn, State, _Extra) ->
 
 % Private
 
+check_username_password(Username, Password, #state{users=Users}) ->
+  case orddict:find(Username, Users) of
+    {ok, {PasswordHash, Pepper}} ->
+      case do_hash(Password, Pepper) of
+        PasswordHash -> ok;
+        _            -> error
+      end;
+    error -> error
+  end.
+
+do_hash(V, Pepper) ->
+  erlang:phash2(<< V/binary, $:, Pepper/binary, $:, ?SALT/binary >>).
+
+store_username_password(Username, Password, State=#state{users=Users}) ->
+  Pepper = crypto:strong_rand_bytes(8),
+  PasswordHash = do_hash(Password, Pepper),
+  NewUsers = orddict:store(Username, {PasswordHash, Pepper}, Users),
+  NewState = State#state{users=NewUsers},
+  NewState.
+
+get_expiration() ->
+  integer_to_binary(bam_lib:minutes_from_now(60)).
--- apps/bam_lib/src/bam_lib.erl
+++ apps/bam_lib/src/bam_lib.erl
@@ -1,6 +1,13 @@
 -module(bam_lib).
 
--export([now_milliseconds/0]).
+-export([
+  now_milliseconds/0,
+  days_from_now/1,
+  hours_from_now/1,
+  minutes_from_now/1,
+  seconds_from_now/1
+  ]).
+
 -export([bin_to_hex/1]).
 
 %% API
@@ -9,5 +16,19 @@ now_milliseconds() ->
   {Mega, Sec, Micro} = os:timestamp(),
   (Mega*1000000 + Sec)*1000 + round(Micro/1000).
 
+days_from_now(N)    -> now_offset(N*24*60*60).
+hours_from_now(N)   -> now_offset(N*60*60).
+minutes_from_now(N) -> now_offset(N*60).
+seconds_from_now(N) -> now_offset(N).
+
 bin_to_hex(B) ->
   bam_lib_bin_to_hex:bin_to_hex(B).
+
+%% Private
+
+now_offset(N) ->
+  Now = now_milliseconds(),
+  now_offset(Now, N).
+
+now_offset(Now, N) ->
+  Now + N*1000.
